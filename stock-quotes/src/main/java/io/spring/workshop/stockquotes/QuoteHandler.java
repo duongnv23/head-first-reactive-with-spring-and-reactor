525c75899e9146f850f2e352361e80ce53a40000
darius <duongnvit23@gmail.com>
2020-07-17 22:09:44 +0700

    complete

--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
@@ -1,26 +1,22 @@
 package io.spring.workshop.stockquotes;
 
-import reactor.core.publisher.Mono;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.http.MediaType.APPLICATION_STREAM_JSON;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
+@RequiredArgsConstructor
 public class QuoteHandler {
+    private final QuoteGenerator generator;
 
-	private final QuoteGenerator quoteGenerator;
-
-	public QuoteHandler(QuoteGenerator quoteGenerator) {
-		this.quoteGenerator = quoteGenerator;
-	}
-
-	public Mono<ServerResponse> streamQuotes(ServerRequest request) {
-		return ok()
-				.contentType(APPLICATION_STREAM_JSON)
-				.body(this.quoteGenerator.fetchQuoteStream(), Quote.class);
-	}
+    public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+        return ok()
+                .contentType(APPLICATION_STREAM_JSON)
+                .body(this.generator.fetchQuoteStream(), Quote.class);
+    }
 }
